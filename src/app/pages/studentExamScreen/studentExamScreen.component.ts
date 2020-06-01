6e114238ff301f7797279ef5be2f0a313c2e2bc4
xiao-xuan <s11037018@gmail.com>
2020-06-01 13:06:11 +0800

    add studentExamScreen page

---
+++ src/app/pages/studentExamScreen/studentExamScreen.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+
+interface MyVideoElement extends HTMLVideoElement {
+  requestPictureInPicture(): any;
+}
+
+interface MyDocument extends Document {
+  exitPictureInPicture(): any;
+}
+
+@Component({
+  selector: 'app-studentExamScreen',
+  moduleId: module.id,
+  templateUrl: 'studentExamScreen.component.html'
+})
+
+
+export class StudentExamScreenComponent implements OnInit {
+  isShownAlert: Boolean = false;
+  ngOnInit() {
+    const video = <MyVideoElement>document.querySelector('#videoElement');
+
+    let flag = true;
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({video: true})
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+          console.log('Something went wrong!');
+        });
+    }
+
+    $('#pip-request').on('click', () => {
+      if (flag) {
+        video.requestPictureInPicture();
+        flag = false;
+      } else {
+        const mydocument = <MyDocument>document;
+        mydocument.exitPictureInPicture();
+        flag = true;
+      }
+    })
+  }
+
+}
